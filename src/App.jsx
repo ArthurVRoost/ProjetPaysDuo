e8158a7232a4916aeafa69a684b9f8f57b490a69
Emil <somaleyoo123@gmail.com>
2025-06-05 10:15:25 +0200

    search & select

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './Pages/Home/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,11 +7,12 @@ export default function Home(pros) {
 const[data,setData]=useState([])
 const [loading, setLoading] = useState(true)
 const [error, setError] = useState(null)
+const[recherche,setRecherche]=useState("")
        useEffect(() => {
             axios.get("https://fakestoreapi.com/products/")
-                .then(response => response.json())
-                .then((products) => {
-                    setData(products)
+              
+                .then((response) => {
+                    setData(response.data)
                     setLoading(false)
                 })
                 .catch((error) => {
@@ -19,9 +20,26 @@ const [error, setError] = useState(null)
                     setLoading(false)
                 })
         }, [])
-
+    
+    if (loading) return <div className="text-center p-4">Chargement...</div>
+    if (error) return <div className="text-center p-4 text-red-500">Erreur: {error}</div>
+    
     return(
         <>
+        <div className="header">
+            <input type="search" placeholder='Sreach for a country...' />
+            <div class="dropdown">
+                <select>
+                    <option disabled selected>Filter by Region</option>
+                    <option value="africa">Africa</option>
+                    <option value="americas">Americas</option>
+                    <option value="asia">Asia</option>
+                    <option value="europe">Europe</option>
+                    <option value="oceania">Oceania</option>
+                </select>
+            </div>
+        </div>
+            
 
         </>
     )
